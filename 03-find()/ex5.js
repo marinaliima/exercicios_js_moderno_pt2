24b0c2b7c80e65076e21847621c37ef53ae7b2e4
marinaliima <marina.l@edu.senai.br>
2026-09-24 11:17:20 -0300

    feat: adiciona exercícios com find()

---
+++ 03-find()/ex5.js
@@ -0,0 +1,12 @@
+// 5. Encontre o produto com id === 3.
+
+const produtos = [
+    { id: 1, nome: "Notebook", preco: 3500, estoque: 5, ativo: true },
+    { id: 2, nome: "Mouse", preco: 80, estoque: 0, ativo: true },
+    { id: 3, nome: "Teclado", preco: 150, estoque: 10, ativo: false },
+    { id: 4, nome: "Monitor", preco: 1200, estoque: 3, ativo: true }
+];
+
+const encontraProduto = produtos.find((p) => p.id === 3);
+
+console.log(encontraProduto);
